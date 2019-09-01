b4822521f84dfbe53cb32403d4b47e200db0abae
xielin <xielin.mail@qq.com>
2019-09-01 17:25:13 +0800

    实验室电脑代码写完了

--- day07-code/src/cn/itcast/demo07/Demo01.java
+++ day07-code/src/cn/itcast/demo07/Demo01.java
@@ -3,5 +3,6 @@ package cn.itcast.demo07;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("笔记本上的代码推送到远端！");
+        System.out.println("实验室电脑上的代码上的代码推送到远端！");
     }
 }
