f4637a304f267ffe8c128e5eb18079174ddae57f
xie <xielin.mail@qq.com>
2019-09-02 23:30:34 +0800

    明天实验室拉下来！

--- day07-code/src/cn/itcast/day07/demo01/Demo01.java
+++ day07-code/src/cn/itcast/day07/demo01/Demo01.java
@@ -3,5 +3,6 @@ package cn.itcast.day07.demo01;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("输出一段代码");
+        System.out.println("笔记本电脑已经写出了Java文件，明天去实验室去测试在实验室是否能pull运行！");
     }
 }
