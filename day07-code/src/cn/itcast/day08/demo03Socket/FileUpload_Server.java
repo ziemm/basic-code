2c30c10cb2658ce04b906611bd738b36cf966322
xielin <xielin.mail@qq.com>
2019-10-10 22:27:49 +0800

    bsSocket

--- day07-code/src/cn/itcast/day08/demo03Socket/FileUpload_Server.java
+++ day07-code/src/cn/itcast/day08/demo03Socket/FileUpload_Server.java
@@ -1,9 +1,6 @@
 package cn.itcast.day08.demo03Socket;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,21 +13,51 @@ public class FileUpload_Server {
         System.out.println("服务器启动...");
         ServerSocket ss =new ServerSocket(6666);
 
-        //接收来自客户端的对话
-        Socket server = ss.accept();
+        //为了提高上传的个数，需要使用多线程，每创建一个socket对象，就使用一个线程接收
+        while(true){
+            //接收来自客户端的对话
+            Socket server = ss.accept();
+            //使用匿名内部类或者Lambda表达式
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //获得输入流，并且加上缓冲
+                        BufferedInputStream bis = new BufferedInputStream(server.getInputStream());
+                        //获得输出流，输出到服务器的文件夹内
 
-        //获得输入流，并且加上缓冲
-        BufferedInputStream bis = new BufferedInputStream(server.getInputStream());
-        //获得输出流，输出到服务器的文件夹内
+                        //数据接收并写入服务器上的文件
+                        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(
+                                "day07-code\\upload\\"+System.currentTimeMillis()+" copy.jpg"));
+                        byte[] b = new byte[8*1024];
+                        int len = 0;
+                        while((len=bis.read(b))!=-1){
+                            bos.write(b,0,len);
+                        }
 
-        //数据接收并写入服务器上的文件
-        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(
-                              "basic-code\\day07-code\\upload\\"+System.currentTimeMillis()+" copy.jpg"));
-        byte[] b = new byte[8*1024];
-        int len = 0;
-        while((len=bis.read(b))!=-1){
+                        //告诉客户端文件上传完毕
+                        System.out.println("back...");
+                        OutputStream os = server.getOutputStream();
+                        os.write("上传成功！".getBytes());
 
+                        os.close();
+
+                        //关闭资源
+                        bos.close();
+                        bis.close();
+                        server.close();
+                        System.out.println("文件已上传！");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
         }
+
+
+
+
+
 
 
     }
