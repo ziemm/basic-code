7d9d778e5227e0baf19234aeb04a6d2e5c499005
xielin <xielin.mail@qq.com>
2019-10-30 22:59:16 +0800

    servlet

---
+++ day13_servlet/src/cn/itcast/web/servlet/ServletDemo.java
@@ -0,0 +1,39 @@
+package cn.itcast.web.servlet;
+
+import javax.jws.WebService;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * 注解使用
+ * @author: xie
+ * @create: 2019-10-30 21:46
+ **/
+@WebServlet("/demo3")
+public class ServletDemo implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+       System.out.println("servlet3.0来了...");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
