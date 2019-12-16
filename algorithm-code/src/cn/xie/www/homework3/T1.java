535b72e9ef9d292623e930ac24f86caf4a47b136
xielin <xielin.mail@qq.com>
2019-12-16 12:41:38 +0800

    homework

---
+++ algorithm-code/src/cn/xie/www/homework3/T1.java
@@ -0,0 +1,78 @@
+package cn.xie.www.homework3;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import static java.lang.Math.min;
+import static sun.swing.MenuItemLayoutHelper.max;
+
+/**
+ * @author: xie
+ * @create: 2019-12-09 14:19
+ **/
+public class T1 {
+    private static char[][] mp = new char[1004][1004];
+    static int n,m,ans =0;
+    static int[] dx ={0,1,1,-1};
+    static int[] dy ={1,0,-1,0};
+    static int gg=0;
+
+    static private Queue<Integer> qx= new LinkedList<>();
+    static private Queue<Integer> qy= new LinkedList<>();
+
+    public static void bfs(int a,int b){
+        qx.offer(a);
+        qy.offer(b);
+        int lp =a,rp=b,gt=a,kt=b,g=1;
+        mp[a][b]='.';
+        while(!qx.isEmpty()){
+            int x = qx.element();
+            int y = qx.element();
+            qx.poll();
+            qy.poll();
+            for (int i = 0; i < 4; i++) {
+                int nowx=x+dx[i];
+                int nowy=y=dy[i];
+                if(nowx>0&&nowx<=n&&nowy>0&&nowy<=m&&mp[nowx][nowy]=='#'){//广度优先遍历的外加条件：是否是船且是否相互接触
+                    qx.offer(nowx);
+                    qy.offer(nowy);
+                    ++g;
+                    mp[nowx][nowy]='.';
+                    lp=min(lp,nowx);gt=max(gt,nowx);
+                    rp=min(rp,nowy);kt=max(kt,nowy);
+                }
+            }
+        }
+        if((gt-lp+1)*(kt-rp+1) !=g) gg=1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n =sc.nextInt();
+        m =sc.nextInt();
+        //赋值
+        for (int i = 1; i <= n; i++) {
+            char[] temp = sc.next().toCharArray();
+            for (int j = 0; j < temp.length; j++) {
+                mp[i][j+1]=temp[j];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <=m ; j++) {
+                if (mp[i][j]=='#'){
+                    gg=0;
+                    ++ans;
+                    bfs(i,j);
+                    if(gg==1){
+                        System.out.println("Bad placement.");
+                    }
+                }
+            }
+        }
+        System.out.println("There are "+ans+" ships");
+    }
+
+
+}
---
+++ day07-code/src/cn/itcast/day07/demo06_Map/Iterator.java
@@ -0,0 +1,58 @@
+package cn.itcast.day07.demo06_Map;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+/**
+ * @author: xie
+ * @create: 2019-11-26 20:56
+ **/
+public class Iterator {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add(null);
+        list.add("abc1");
+        list.add("abc2");
+        list.add("abc3");
+        list.add("abc4");
+
+        /*
+         * 使用迭代器遍历集合
+         */
+        //获取迭代器
+        java.util.Iterator<String> it = list.iterator();
+        //使用while遍历集合
+        while (it.hasNext()) {
+            String s = it.next();
+
+            /*
+             * 判断集合中有没有"abc3"这个元素
+             * 如果有,增加一个元素"itcast"
+             * 编程技巧:使用equals判断的时候,要把已知的变量写在前边,未知的写在后边,防止空指针异常
+             */
+            //if(s.equals("abc3")){
+            if ("abc3".equals(s)) {
+                //1.迭代就是迭代,不要对集合进行修改
+                //list.add("itcast");
+            }
+
+            System.out.println(s);
+        }
+
+        System.out.println("------------------");
+
+         //2.使用迭代器Iterator的子接口ListIterator中的方法add/remove,让迭代器自己增加往集合中增加元素/移除元素
+
+        ListIterator<String> listIt = list.listIterator();
+        while(listIt.hasNext()){
+            String s = listIt.next();
+            if("abc3".equals(s)){
+                listIt.add("itcast");
+            }
+            System.out.println(s);
+        }
+        System.out.println(list);
+    }
+
+}
